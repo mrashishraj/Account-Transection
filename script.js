6f26ce9b33ccf783e8d4ab969365b1b9b2b005f4
Ashish Raj <mr.ashishrajyadav@gmail.com>
2020-06-18 12:04:35 +0530

    table function complete

--- script.js
+++ script.js
@@ -1,8 +1,38 @@
+var data = []
+
 window.onload = function () {
     var form = document.querySelector("form")
     form.addEventListener('submit', function () {
         event.preventDefault();
-
-
+        var element = event.target;
+        var record = {
+            status: false,
+            item: element[0].value,
+            quantity: element[1].value,
+            id: data.length + 1
+        }
+        data.push(record);
+        console.log(data);
+        submitData(record)
     })
+
+}
+
+function submitData(record) {
+    var row = document.createElement("tr");
+
+    var itemElement = document.createElement("td")
+    itemElement.textContent = record.item;
+    var quantityElement = document.createElement("td")
+    quantityElement.textContent = record.quantity;
+
+    row.appendChild(itemElement);
+    row.appendChild(quantityElement);
+
+    var table = document.getElementById("menu")
+    table.appendChild(row)
+
+
+
+
 }
